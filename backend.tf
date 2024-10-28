b03d61bd92eb10b5cf40fc967eea37bc8337fc55
kumarTarungedala <tarunkumar39951@gmail.com>
2024-10-28 10:10:51 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tarun.project"
+    bucket = "tarunmukesh"
     key = "path/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "statelock"
